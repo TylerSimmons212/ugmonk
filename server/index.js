61bda2e3dd82bff96dc381297aa904773c846609
Tyler Simmons <tylersimmons@Tylers-MacBook-Pro.local>
2019-02-28 10:43:19 -0700

    updated for hosting

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const path = require('path'); // Usually moved to the start of file
 require("dotenv").config();
 const express = require("express");
 const massive = require("massive");
@@ -9,8 +10,11 @@ const { SERVER_PORT, DB_CONNECTION, SESSION_SECRET } = process.env;
 
 const app = express();
 
+
 app.use(express.json());
 app.use(require("body-parser").text());
+
+app.use( express.static( `${__dirname}/../build` ) );
 
 app.post("/charge", async (req, res) => {
   try {
@@ -20,7 +24,7 @@ app.post("/charge", async (req, res) => {
       description: "An example charge",
       source: req.body
     });
-
+    
     res.json({status});
   } catch (err) {
     res.status(500).end();
@@ -38,7 +42,7 @@ app.use(sessions({
 massive(DB_CONNECTION).then(db => {
   app.set('db', db);
   app.listen(SERVER_PORT, () =>
-    console.log(`Houston we have lift off on port ${SERVER_PORT}`)
+  console.log(`Houston we have lift off on port ${SERVER_PORT}`)
   );
 });
 
@@ -51,3 +55,8 @@ app.post('/auth/logout', ctrl.logout);
 app.post('/api/submit', ctrl.submit);
 app.get('/api/user', ctrl.getUser);
 app.delete(`/api/delete/:id`, ctrl.delete);
+
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+});
--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import App from './App';
 import {HashRouter as Router} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import store from './ducks/store';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Provider store={store}><Router><App /></Router></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
  
